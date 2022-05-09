d41b13435dd462a61656d9bff1db1f88dd3d4f47
RodolfoBaume <robalaz@hotmail.com>
2022-05-09 17:27:49 -0500

    Se agrega endpoint PUT localhost:3000/missionCommanders

--- server.js
+++ server.js
@@ -132,6 +132,21 @@ app.get('/missionCommanders', async (req, res) => {
     return res.json({message});
   });
 
+  app.put('/missionCommanders/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+
+	await prisma.missionCommander.update({
+		where: {
+			id: id
+		},
+		data: {
+			mainStack: req.body.mainStack
+		}
+	})
+
+	return res.json({message: "Actualizado correctamente"});
+});
+
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
